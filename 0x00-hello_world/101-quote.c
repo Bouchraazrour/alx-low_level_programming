3c5b71d4319aee502a83dcdc0716c65ad8f924da
Bouchraazrour <bouchraazrour@gmail.com>
2023-03-19 10:34:26 -0700

    d

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 int main(void)
 {
 	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    	write(STDERR_FILENO, message, 42);
-    	exit(1);
+	write(STDERR_FILENO, message, 59);
+	exit(1);
 }
