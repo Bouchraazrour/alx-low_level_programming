c5fbb49767cd2af7226c28306b98b618d3114a85
Bouchraazrour <bouchraazrour@gmail.com>
2023-03-20 05:15:17 -0700

    d

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stlib.h>
+/**
+ * main - A program that assign a random number to n
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX/2;
+	if ( n>0 )
+	{
+		printf("%d is positive", n);
+	}
+	else if ( n<0 )
+	{
+		printf("%d is negative", n);
+	}
+	else
+	{
+		printf("%d is zero", n);
+	}
+	return (0);
+}
