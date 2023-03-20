d95ed02e0a0026a6862a36c30c06ad7d919960fc
Bouchraazrour <bouchraazrour@gmail.com>
2023-03-20 05:41:29 -0700

    d

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("%d is positive ", n);
+		printf("%d is positive", n);
 	}
 	else if (n < 0)
 	{
-		printf("%d is negative ", n);
+		printf("%d is negative", n);
 	}
 	else
 	{
-		printf("%d is zero ", n);
+		printf("%lu is zero", n);
 	}
 	return (0);
 }
