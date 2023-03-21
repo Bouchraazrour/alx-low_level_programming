2c817c4a029b830e00ba1d0960537e8995c1152b
Bouchraazrour <bouchraazrour@gmail.com>
2023-03-21 11:24:56 -0700

    d

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - a program that prints the alphabet
+ * Return: 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
