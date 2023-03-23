717ae65c9d49e07efac8dba9d0cfa84303ae06b2
Bouchraazrour <bouchraazrour@gmail.com>
2023-03-23 07:55:41 -0700

    d

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.c"
+/**
+ * _islowe -  a function that checks for lowercase character
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
