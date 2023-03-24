8b2eec9e88edb2744c7b8aee9e7e66826dfe73b3
Bouchraazrour <bouchraazrour@gmail.com>
2023-03-24 14:07:22 -0700

    d

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,16 +7,16 @@ int main(void)
 {
 	int i;
 
-	print("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
+	
 	/*while (i < 10)*/
 	/*{*/
-	/*	putchar(i);*/
+	/*putchar(i);*/
 	/*}*/
-
+	
 	printf("Infinite loop avoided! \\o/\n");
-
+	
 	return (0);
 }
